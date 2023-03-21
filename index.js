64fbd6e82b67a5e581fa510d5a122af82b09eee7
Andrey <antonov.andrey.10101@gmail.com>
2023-03-21 15:53:54 +0300

    feat: сonnect JSON Web Token (JWT)

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
@@ -10,8 +11,18 @@ app.get("/", (req, res) => {
 
 app.post("/auth/login", (req, res) => {
   console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Вася Пупкин",
+    },
+    "secret123"
+  );
+
   res.json({
     success: true,
+    token
   });
 });
 
